97129e87267fe551256c69d36f9cf4d5404fc94e
Manoj-Kumar-Munda <manojkrmunda828@gmail.com>
2026-01-30 19:43:12 +0530

    feat: add subscription api services

---
+++ services/subscription/subscription.service.ts
@@ -0,0 +1,57 @@
+import { endpoints } from '@/config/endpoints';
+import api from '@/lib/api';
+import { useMutation, useQuery } from '@tanstack/react-query';
+import { ApiResponse } from '../types';
+import { getQueryClient } from '@/lib/query-client';
+
+interface ISubscription {
+  _id: string;
+  channelInfo: {
+    _id: string;
+    fullName: string;
+    avatar: string;
+    username: string;
+    totalSubs: number;
+  };
+}
+interface ISubscriptionsResponse {
+  data: ISubscription[];
+}
+const useSubscribers = (channelId: string) => {
+  return useQuery({
+    queryKey: [endpoints.subscriptions.subscribers.queryKey, channelId],
+    queryFn: () =>
+      api.get(
+        endpoints.subscriptions.subscribers.url.replace(
+          '{channelId}',
+          channelId
+        )
+      ),
+    enabled: !!channelId,
+  });
+};
+
+const useSubscriptions = (userId: string = '') => {
+  return useQuery<ApiResponse<ISubscriptionsResponse>>({
+    queryKey: [endpoints.subscriptions.list.queryKey, userId],
+    queryFn: () =>
+      api.get(endpoints.subscriptions.list.url.replace('{userId}', userId)),
+    enabled: !!userId,
+  });
+};
+
+const useToggleSubscription = () => {
+  return useMutation({
+    mutationFn: (channelId: string) =>
+      api.post(
+        endpoints.subscriptions.toggle.url.replace('{channelId}', channelId)
+      ),
+    onSettled: () => {
+      getQueryClient().invalidateQueries({
+        queryKey: [endpoints.subscriptions.list.queryKey],
+      });
+    },
+  });
+};
+
+export { useSubscribers, useSubscriptions, useToggleSubscription };
