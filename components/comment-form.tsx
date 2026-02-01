269cc3634a6e42f969d905da21d04e512358f391
Manoj-Kumar-Munda <manojkrmunda828@gmail.com>
2026-02-01 22:50:29 +0530

    refactor: remove redundant code and improve redabilty

--- components/comment-form.tsx
+++ components/comment-form.tsx
@@ -17,11 +17,9 @@ const CommentForm = ({
   onCancel,
   disabled,
 }: ICommentFormProps) => {
-  const [showButtons, setShowButtons] = useState(false);
   const [comment, setComment] = useState('');
 
   const handleCancel = () => {
-    setShowButtons(false);
     setComment('');
     onCancel?.();
   };
@@ -35,31 +33,29 @@ const CommentForm = ({
       <Input
         placeholder="Add a comment..."
         className="border-0 rounded-none text-sm border-b border-neutral-700 ring-0 shadow-none focus:ring-0 focus-within:ring-0 focus-visible:ring-0"
-        onFocus={() => setShowButtons(true)}
         value={comment}
         autoFocus={true}
         onChange={(e) => setComment(e.target.value)}
         disabled={disabled}
       />
-      {showButtons && (
-        <div className="flex justify-end gap-2">
-          <Button
-            variant="ghost"
-            className="rounded-full px-4 text-xs h-8"
-            onClick={handleCancel}
-            disabled={disabled}
-          >
-            Cancel
-          </Button>
-          <Button
-            className="rounded-full px-4 text-xs h-8"
-            onClick={handleSubmit}
-            disabled={disabled}
-          >
-            {label}
-          </Button>
-        </div>
-      )}
+
+      <div className="flex justify-end gap-2">
+        <Button
+          variant="ghost"
+          className="rounded-full px-4 text-xs h-8"
+          onClick={handleCancel}
+          disabled={disabled}
+        >
+          Cancel
+        </Button>
+        <Button
+          className="rounded-full px-4 text-xs h-8"
+          onClick={handleSubmit}
+          disabled={disabled}
+        >
+          {label}
+        </Button>
+      </div>
     </div>
   );
 };
