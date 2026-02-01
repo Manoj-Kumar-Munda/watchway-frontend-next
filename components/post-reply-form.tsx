9bc8450277fb2e48dc941acf3c6d581dc79b37dd
Manoj-Kumar-Munda <manojkrmunda828@gmail.com>
2026-02-01 22:51:18 +0530

    feat: create PostReply component

---
+++ components/post-reply-form.tsx
@@ -0,0 +1,42 @@
+import CommentForm from '@/components/comment-form';
+import { usePostCommentMutation } from '@/services/community/community.service';
+import { useUserStore } from '@/store';
+import { toast } from 'sonner';
+
+interface IPostReplyFormProps {
+  postId: string;
+  onCancel?: () => void;
+}
+
+const PostReplyForm = ({ postId, onCancel }: IPostReplyFormProps) => {
+  const userId = useUserStore((state) => state.user?._id);
+  const { mutateAsync: postComment, isPending } = usePostCommentMutation(
+    userId!,
+    postId
+  );
+
+  const handleSubmit = (comment: string) => {
+    postComment(
+      { content: comment },
+      {
+        onError: () => {
+          toast.error('Failed to post comment');
+        },
+        onSuccess: () => {
+          toast.success('Comment posted successfully');
+          onCancel?.();
+        },
+      }
+    );
+  };
+  return (
+    <CommentForm
+      label="Reply"
+      onSubmit={handleSubmit}
+      onCancel={onCancel}
+      disabled={isPending}
+    />
+  );
+};
+
+export default PostReplyForm;
